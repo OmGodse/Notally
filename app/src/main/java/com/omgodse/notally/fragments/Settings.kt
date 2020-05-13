e16d85eca4e993d996b3ac2c3ba0126d7e63e304
Om Godse <omgodsea@gmail.com>
2020-05-13 20:00:29 +0530

    Removed hardcoded libraries

--- app/src/main/java/com/omgodse/notally/fragments/Settings.kt
+++ app/src/main/java/com/omgodse/notally/fragments/Settings.kt
@@ -11,7 +11,6 @@ import androidx.preference.ListPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.textview.MaterialTextView
 import com.omgodse.notally.R
 
 class Settings : PreferenceFragmentCompat() {
@@ -41,44 +40,24 @@ class Settings : PreferenceFragmentCompat() {
         }
 
         ratePref?.setOnPreferenceClickListener {
-            openLink(AppLink)
+            openLink(PlayStore)
             return@setOnPreferenceClickListener true
         }
 
         librariesPref?.setOnPreferenceClickListener {
-            val libraries = View.inflate(mContext, R.layout.dialog_about, null)
-            val iText: MaterialTextView = libraries.findViewById(R.id.iText)
-            val jsoup: MaterialTextView = libraries.findViewById(R.id.Jsoup)
-            val prettyTime: MaterialTextView = libraries.findViewById(R.id.PrettyTime)
-            val materialComponents: MaterialTextView = libraries.findViewById(R.id.MaterialComponentsAndroid)
-
+            val libraries = arrayOf("iText", "Jsoup", "Pretty Time", "Material Components for Android")
             val builder = MaterialAlertDialogBuilder(mContext)
             builder.setTitle(R.string.libraries)
-            builder.setView(libraries)
-
+            builder.setItems(libraries) { dialog, which ->
+                when (which) {
+                    0 -> openLink(iText)
+                    1 -> openLink(Jsoup)
+                    2 -> openLink(PrettyTime)
+                    3 -> openLink(MaterialComponents)
+                }
+            }
             builder.setNegativeButton(R.string.cancel, null)
-            val dialog = builder.create()
-
-            jsoup.setOnClickListener {
-                openLink(JsoupLink)
-            }
-
-            iText.setOnClickListener {
-                dialog.dismiss()
-                openLink(iTextLink)
-            }
-
-            prettyTime.setOnClickListener {
-                dialog.dismiss()
-                openLink(PrettyTimeLink)
-            }
-
-            materialComponents.setOnClickListener {
-                dialog.dismiss()
-                openLink(MaterialComponentsLink)
-            }
-
-            dialog.show()
+            builder.show()
             return@setOnPreferenceClickListener true
         }
 
@@ -112,10 +91,10 @@ class Settings : PreferenceFragmentCompat() {
 
     companion object {
         private const val Github = "https://github.com/OmGodse/Notally"
-        private const val AppLink = "https://play.google.com/store/apps/details?id=com.omgodse.notally"
-        private const val iTextLink = "https://github.com/itext/itextpdf"
-        private const val JsoupLink = "https://github.com/jhy/jsoup/"
-        private const val PrettyTimeLink = "https://github.com/ocpsoft/prettytime"
-        private const val MaterialComponentsLink = "https://github.com/material-components/material-components-android"
+        private const val PlayStore = "https://play.google.com/store/apps/details?id=com.omgodse.notally"
+        private const val iText = "https://github.com/itext/itextpdf"
+        private const val Jsoup = "https://github.com/jhy/jsoup/"
+        private const val PrettyTime = "https://github.com/ocpsoft/prettytime"
+        private const val MaterialComponents = "https://github.com/material-components/material-components-android"
     }
 }
