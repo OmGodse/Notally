62681a13a7971f714cda49789278b1625a2a6191
Om Godse <omgodsea@gmail.com>
2020-06-04 11:22:54 +0530

    Removed redundant equals and hash code method in data class

--- app/src/main/java/com/omgodse/notally/miscellaneous/Note.kt
+++ app/src/main/java/com/omgodse/notally/miscellaneous/Note.kt
@@ -9,26 +9,6 @@ data class Note (val isNote: Boolean,
                  val labels: HashSet<String>,
                  val filePath: String) {
 
-    override fun equals(other: Any?): Boolean {
-        return when (other) {
-            null -> false
-            is Note -> other.hashCode() == hashCode()
-            else -> false
-        }
-    }
-
-    override fun hashCode(): Int {
-        var result = isNote.hashCode()
-        result = 31 * result + title.hashCode()
-        result = 31 * result + timestamp.hashCode()
-        result = 31 * result + body.hashCode()
-        result = 31 * result + items.hashCode()
-        result = 31 * result + spans.hashCode()
-        result = 31 * result + labels.hashCode()
-        result = 31 * result + filePath.hashCode()
-        return result
-    }
-
     fun isEmpty(): Boolean {
         return if (isNote){
             title.isBlank() && body.isBlank()
