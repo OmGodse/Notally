03c38a82a9d9fe38e8bebb6195c922385bd1b9a6
Om Godse <64728411+OmGodse@users.noreply.github.com>
2024-12-11 23:41:01 +0530

    Fixed action mode transition glitch on Lollipop

--- app/src/main/java/com/omgodse/notally/activities/MainActivity.kt
+++ app/src/main/java/com/omgodse/notally/activities/MainActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.print.PostPDFGenerator
-import android.transition.TransitionManager
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -26,8 +25,9 @@ import androidx.navigation.navOptions
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.transition.TransitionManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.google.android.material.transition.platform.MaterialFade
+import com.google.android.material.transition.MaterialFade
 import com.omgodse.notally.MenuDialog
 import com.omgodse.notally.R
 import com.omgodse.notally.databinding.ActivityMainBinding
